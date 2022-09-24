0b4310cab8fdcbfc41e7a3c47cad0732f43d694d
Hasan Almujtaba <hasan.almujtaba01@gmail.com>
2022-09-24 13:29:39 +0700

    refactor category list

--- components/CategoryList/index.tsx
+++ components/CategoryList/index.tsx
@@ -1,63 +1,34 @@
-import { Box, Grid, Heading, Skeleton, SkeletonText } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import { useGetMealCategories } from 'hooks/fetch'
-import CategoryItem from '../CategoryItem'
+import CardGroup from '../CardGroup'
+import CardGroupSkeleton from '../CardGroupSkeleton'
+import CardItem from '../CardItem'
 
 const CategoryList = () => {
   const { data, isSuccess } = useGetMealCategories()
 
-  if (isSuccess) {
-    return (
-      <Box py="20">
-        <Heading textAlign="center">Meal Categories</Heading>
-        <Grid
-          mt="5"
-          templateColumns={[
-            'repeat(1, 1fr)',
-            'repeat(2, 1fr)',
-            'repeat(3, 1fr)',
-            'repeat(4, 1fr)',
-          ]}
-          gap={6}
-        >
-          {data.map((item, index) => (
-            <CategoryItem
-              title={item.strCategory}
-              thumbnail={item.strCategoryThumb}
-              key={index}
-            />
-          ))}
-        </Grid>
-      </Box>
+  let content = <CardGroupSkeleton />
+
+  if (isSuccess)
+    content = (
+      <CardGroup>
+        {data.map((item, index) => (
+          <CardItem
+            id={item.idCategory}
+            identifier="title"
+            link="category"
+            title={item.strCategory}
+            thumbnail={item.strCategoryThumb}
+            key={index}
+          />
+        ))}
+      </CardGroup>
     )
-  }
 
   return (
     <Box py="20">
       <Heading textAlign="center">Meal Categories</Heading>
-      <Grid
-        mt="5"
-        templateColumns={[
-          'repeat(1, 1fr)',
-          'repeat(2, 1fr)',
-          'repeat(3, 1fr)',
-          'repeat(4, 1fr)',
-        ]}
-        gap={6}
-      >
-        {[...Array(12)].map((_, index) => (
-          <Box key={index}>
-            <Skeleton
-              height="270"
-              rounded="xl"
-            ></Skeleton>
-            <SkeletonText
-              skeletonHeight="4"
-              noOfLines={1}
-              mt="2"
-            />
-          </Box>
-        ))}
-      </Grid>
+      {content}
     </Box>
   )
 }
