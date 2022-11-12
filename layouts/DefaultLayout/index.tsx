fc7286564c57bd610aaefa12d63a1f4968a026b7
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-11-12 19:07:28 +0700

    fix dark mode

--- layouts/DefaultLayout/index.tsx
+++ layouts/DefaultLayout/index.tsx
@@ -3,9 +3,11 @@ import { DefaultLayout } from 'types/layout'
 
 const DefaultLayout = ({ children }: DefaultLayout) => {
   return (
-    <div className="container">
-      <Navbar />
-      {children}
+    <div className="dark:bg-zinc-800 dark:text-white">
+      <div className="container">
+        <Navbar />
+        {children}
+      </div>
     </div>
   )
 }
